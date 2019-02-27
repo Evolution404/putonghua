73876619e0223a39a24584a3e8772315c67196bb
zhangyuxi <615598813.com>
2019-02-27 18:01:28 +0800

    院系映射以及核对成功的提示

--- static/js/confirm.js
+++ static/js/confirm.js
@@ -24,7 +24,7 @@ new Vue({
                 self.form.profession = data['profession'];
                 self.form.nation = data['nation'];
                 self.form.workUnit = data['workUnit'];
-                self.form.department = data['department'];
+                self.form.department = self.parseDepartment(data['department']);
                 self.form.classId = data['classId'];
                 self.form.rank = data['rank'];
                 self.hasImg = data['hasImg'];
@@ -45,6 +45,26 @@ new Vue({
       }
     },
     methods: {
+      parseDepartment(deptId){
+        let deptMap = {
+          '01':'汽车工程学院',
+          '02':'信息科学与工程学院',
+          '03':'经济管理学院',
+          '04':'计算机科学与技术学院',
+          '05':'语言文学学院',
+          '06':'理学院',
+          '07':'海洋科学与技术学院',
+          '08':'材料科学与工程学院',
+          '09':'',
+          '10':'理学院',
+          '11':'计算机科学与技术学院',
+          '12':'土木工程系',
+          '13':'船舶与海洋工程学院',
+          '14':'',
+          '15':'语言文学学院',
+        }
+        return deptMap[deptId];
+      },
       handleAvatarSuccess(res, file) {
         this.imageUrl = URL.createObjectURL(file.raw);
         this.hasImg = true;
@@ -75,7 +95,7 @@ new Vue({
       confirmError(){
           this.$message({
                           type: 'warning',
-                          message: '请联系辅导员或相关老师'
+                          message: '请联系5682036进行信息更正'
                       });
       },
       confirmInfo(){
@@ -95,6 +115,9 @@ new Vue({
               axios.post('/api/confirm').then(function(response){
                   data = response.data;
                   if(data.errno === 0){
+                    self.$alert('请以班级为单位进行缴费，缴费要求详见学校网页通知', '报名成功',{
+                      confirmButtonText: '确定'
+                    })
                       self.$message({
                           type: 'success',
                           message: '信息确认成功!'
